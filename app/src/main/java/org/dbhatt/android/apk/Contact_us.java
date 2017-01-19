90459ae17fff36d6e36cbdb264943d601ba2a22b
devdatt bhatt <devsbhattleb@gmail.com>
2017-01-19 11:20:21 +0530

    starting building contact us

---
+++ app/src/main/java/org/dbhatt/android/apk/Contact_us.java
@@ -0,0 +1,18 @@
+package org.dbhatt.android.apk;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dev on 19/1/17.
+ * via this activity user can reach developer
+ */
+
+public class Contact_us extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
